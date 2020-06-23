5b8585d3b0692d7db313e1e78818991a4e2f112e
insoo2846@gmail.com <auddud03^^>
2020-06-23 17:50:16 +0900

    수정#4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Home, Company } from './components'
 
 class App extends Component {
     render() {
         return (
-            <Router>
-                <Route exact path="/" component={Home} />
-                <Route path="/company" component={Company} />
-            </Router>
+            <div>Hellow Nkia!</div>
         )
     }
 }
